9b2018f991cdb945a6948c66e23e3fbfd7a5a5b5
keri19372 <keri19372@gmail.com>
2017-03-15 16:01:37 +0200

    done function setText

---
+++ script/index.js
@@ -0,0 +1,16 @@
+function setText(selector, text){
+
+	var element = document.querySelector(selector);
+	element.innerText = text;
+};
+
+
+function setTextAll(selector, text){
+ 
+ };
+
+
+
+
+//setText("p", "Some is different");
+setTextAll("div block a", "or was it ?");
